c4d6a2c2e4216b0d8a1c228d2a700c99f1493ef2
isangabas <isangabasiofon@gmail.com>
2024-01-29 10:56:30 +0100

    modified:   tst11.py 	modified:   tst18.py

--- tst11.py
+++ tst11.py
@@ -2,7 +2,9 @@ my_list = [1, 2, 3]
 
 print(range(len(my_list)))
 
-
+for i in my_list :
+    del i
+    print(my_list)
 
 
 
--- tst18.py
+++ tst18.py
@@ -1,16 +1,27 @@
 my_list = [1,2,4,4,1,3,2,1]
 my_new_list = []
 
-for i in my_list :
-    for j in range(len(my_list)) :
+for i in range(len(my_list)) :
+    for j in my_list :
         count = 0
-        if i == my_list[j] :
+        if j == my_list[i] :
             count += 1
-            while count >= 2 :
-                del my_list[j]
+            print(count)
+            while count >= 1 :
+                        print(my_list)
         else :
-            my_new_list.append(i)
+            my_new_list.append(j)
             continue
-print(my_new_list)
 
+
+my_list = [1, 2, 4, 4, 1, 3, 2, 1]
+
+unique_list = []
+
+for element in my_list:
+    if element not in unique_list:
+        unique_list.append(element)
+
+print("Original List:", my_list)
+print("List without Repetitions:", unique_list)
             
